5d7b28df4deb8b93513c60bc2c99d0ca1a91f94f
Hack Reactor Student <student@hackreactor.com>
2017-07-21 20:02:12 -0700

    finished http request

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ angular.module('video-player')
   controller: function (youTube) {
     console.log(youTube.search);
     console.log(this.query);
+    var context = this;
     this.currentVideo = window.exampleVideoData[0];
     this.videos = window.exampleVideoData;
     this.selectVideo = (videoClicked) => {
@@ -14,7 +15,10 @@ angular.module('video-player')
       this.videos = newVideos;
       this.currentVideo = newVideos[0];
     };
-    console.log(youTube.search('cow'));
+    console.log(youTube.search('cow', function(vids) {
+      context.videos = vids;
+      context.currentVideo = vids[0];
+    }));
     
   },
   templateUrl: '/src/templates/app.html'
